5beff469a9f07a9e77ceb609cc6f0c3b9d88c7c0
Information Access Division, Geospatial Information Authority of Japan <webmap@gsi.go.jp>
2015-01-07 10:21:59 +0900

    Create reduce.rb

---
+++ reduce.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+require 'fileutils'
+
+last = nil
+geojsonl = nil
+
+def write(geojsonl, path)
+  print "#{path}\t#{geojsonl.join(',')}\n"
+end
+
+while gets
+  r = $_.strip.split("\t")
+  current = r[0]
+  if current != last
+    write(geojsonl, last) unless last.nil?
+    geojsonl = []
+  end
+  geojsonl << r[1]
+  last = current
+end
+write(geojsonl, last)
